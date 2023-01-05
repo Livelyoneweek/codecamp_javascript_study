4ec8613e0fb5bf61282a343e6a1aff83903cabff
Livelyoneweek <wlvvwhs@naver.com>
2023-01-05 23:03:57 +0900

    22230105-실행컨텍스트

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,8 +1,7 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector('#todo-list');
 
 const creteTodo = function() {
-
-    const todoList = document.querySelector('#todo-list');
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
     const newBtn = document.createElement('button');
@@ -11,11 +10,16 @@ const creteTodo = function() {
         newLi.classList.toggle('complete')
     })
 
+    newBtn.addEventListener('dblclick',() => {
+        newLi.remove();
+    })
+
     newSpan.textContent = todoInput.value;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
     todoInput.value='';
+    saveItemsFn();
 }
 
 const keyCodeCheck = function () {
@@ -23,3 +27,30 @@ const keyCodeCheck = function () {
         creteTodo();
     }
 };
+
+const deleteAll = function() {
+    const liList = document.querySelectorAll('li')
+    for(let i = 0; i<liList.length; i++) {
+        liList[i].remove();
+    }
+}
+
+const saveItemsFn = function () {
+    const saveItems = [];
+    
+    for(let i =0; i<todoList.children.length; i++) {
+        // let todo = todoList.children[i].querySelector('span').textContent;
+        let todoObj = {
+            contents: todoList.children[i].querySelector('span').textContent,
+            complete: todoList.children[i].classList.contains('complete')
+        };
+
+        saveItems.push(todoObj);
+    }
+    console.log(saveItems);
+}
+
+
+
+
+
